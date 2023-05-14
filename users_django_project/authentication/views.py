bcd291e9d925d59aad1f898a21429639f0b03307
Dmitry Sannikov <HDiman@users.noreply.github.com>
2023-05-15 01:50:32 +0300

    Не работает settings

--- users_django_project/authentication/views.py
+++ users_django_project/authentication/views.py
@@ -3,7 +3,7 @@ from django.core.mail import send_mail
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import messages
-from users_django_project.users_django_project import settings
+from users_django_project import settings
 
 # Create your views here.
 
