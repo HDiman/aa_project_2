a00144fb1a8c3bd12a526307e8f01ea0bc368d07
Dmitry Sannikov <HDiman@users.noreply.github.com>
2023-05-16 00:12:50 +0300

    Работает settings. Добавил admin.

--- users_django_project/authentication/models.py
+++ users_django_project/authentication/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Users(models.Model):
+    username = models.CharField('Username', max_length=50)
+    first_name = models.CharField('First Name', max_length=250)
+    last_name = models.CharField('Last Name', max_length=250)
+    email = models.CharField('Email', max_length=250)
+    password = models.CharField('Password', max_length=250)
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
