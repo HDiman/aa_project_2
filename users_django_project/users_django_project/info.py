a3384d8fd6c2011fb10f166b483affb262be2ded
Dmitry Sannikov <HDiman@users.noreply.github.com>
2023-05-20 17:04:01 +0300

    Редакция info.py

--- users_django_project/users_django_project/info.py
+++ users_django_project/users_django_project/info.py
@@ -1,5 +1,5 @@
 EMAIL_USE_TLF = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'split777hisense@gmail.com'
-EMAIL_HOST_PASSWORD = 'hisense777'
+EMAIL_HOST_PASSWORD = 'hisense159'
 EMAIL_PORT = 587
